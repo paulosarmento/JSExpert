0fa45be502be726f5bf50196a890b6a238c09b32
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-01-24 21:13:04 -0300

    Dia 5

--- Dia5/app/src/pagination.js
+++ Dia5/app/src/pagination.js
@@ -1,24 +1,31 @@
+// Controla o fluxo das requisições 
+
 const Request = require("./request")
 
 const DEFAULT_OPTIONS = {
+    // maximo de 4 tentativas
     maxRetries: 4,
+    // quanto tempo devo esperar para tentar de novo
     retryTimeout: 1000,
+    // quanto tempo maximo para ele retornar uma requisição
     maxRequestTimeout: 1000,
+    // tempo de intervalo de uma requisição e outra 
     threshold: 200
 }
 
 class Pagination {
     constructor(options = DEFAULT_OPTIONS) {
         this.request = new Request()
-
         this.maxRetries = options.maxRetries
         this.retryTimeout = options.retryTimeout
         this.maxRequestTimeout = options.maxRequestTimeout
         this.threshold = options.threshold
     }
+    // função recursiva utilizamos o reties para quando cair no catch e der o problema ele retornar o erro de novo
     async handleRequest({ url, page, retries = 1 }) {
         try {
             const finalUrl = `${url}?tid=${page}`
+            // dispara uma requisição 
             const result = await this.request.makeRequest({
                 url: finalUrl,
                 method: 'get',
@@ -30,16 +37,20 @@ class Pagination {
         } catch (error) {
             if (retries === this.maxRetries) {
                 console.error(`[${retries}] max retries reached!`)
+                // explode erro final
                 throw error
             }
-
+            // Tentou a primeira vez e deu erro 
             console.error(`[${retries}] an error: [${error.message}] has happened! trying again in ${this.retryTimeout}ms`)
+            // colocamos o await pq retorna uma promise - esperar para tentar outra requisição 
             await Pagination.sleep(this.retryTimeout)
-
+            // tenta de novo e adiciona 1 no retries
             return this.handleRequest({ url, page, retries: retries += 1 })
         }
     }
+    // cria como um membro static pq não precisa olhar para o contexto this
     static async sleep(ms) {
+        // faz o sleep para ele descansar 
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
@@ -61,14 +72,18 @@ class Pagination {
      */
 
     async * getPaginated({ url, page }) {
+        // handle request só é recursivo se der errado // aqui só vem sucesso 
         const result = await this.handleRequest({ url, page })
+        // pego o ultimo id da pagina- se não retornou undefined tenta buscar a propriedade tid = se for undefined ou null retorna 0
         const lastId = result[result.length - 1]?.tid ?? 0
 
         // CUIDADO, mais 1M de requisicoes
         if (lastId === 0) return;
-
+        // retorna o valor para quem chamou 
         yield result
+        // espera para não derrubar a api 
         await Pagination.sleep(this.threshold)
+        // delega a função - retorna o valor pra frente
         yield* this.getPaginated({ url, page: lastId })
 
     }
--- Dia5/app/test/pagination.test.js
+++ Dia5/app/test/pagination.test.js
@@ -17,6 +17,7 @@ describe('Pagination tests', () => {
 
     describe('#Pagination', () => {
         // nao fizemos esses 2 its abaixo, mas  o Erick colocou pra me lembrar que  é legal!
+        //deve ter opções padrão na instância de paginação
         it(`should have default options on Pagination instance`, () => {
             const pagination = new Pagination();
             const expectedProperties = {
@@ -33,7 +34,7 @@ describe('Pagination tests', () => {
             assert.deepStrictEqual(getEntries(pagination), getEntries(expectedProperties))
 
         })
-
+        // deve definir opções padrão na instância de Paginação
         it(`should set default options on Pagination instance`, () => {
             const params = {
                 maxRetries: 2,
@@ -55,62 +56,77 @@ describe('Pagination tests', () => {
 
 
         // daqui pra baixo fizemos juntos!
+        // sleep deve ser um objeto Promise e não retornar valores
         it('#sleep should be a Promise object and not return values', async () => {
 
             const clock = sandbox.useFakeTimers()
             const time = 1
             const pendingPromise = Pagination.sleep(time)
             clock.tick(time)
-
+            //preciso saber se pendingPromise é o objeto promise
             assert.ok(pendingPromise instanceof Promise)
             const result = await pendingPromise
+            // valida se ele é undefined
             assert.ok(result === undefined)
         })
 
         describe('#handleRequest', () => {
-
-            it('should retry an request twice before throing an exception and validate request params and flow', async () => {
+            // deve repetir uma solicitação duas vezes antes de lançar uma exceção e validar os parâmetros e o fluxo da solicitação
+            it('should retry an request twice before throning an exception and validate request params and flow', async () => {
                 const expectedCallCount = 2
                 const expectedTimeout = 10
 
+                // criar pagination
                 const pagination = new Pagination()
+                // sobrescrever  o que ele ja criou
                 pagination.maxRetries = expectedCallCount
                 pagination.retryTimeout = expectedTimeout
                 pagination.maxRequestTimeout = expectedTimeout
 
+                //cria o erro 
                 const error = new Error("timeout")
+                // retorna quantas vezes o método rodou - spy observar uma chamada 
                 sandbox.spy(pagination, pagination.handleRequest.name)
                 sandbox.stub(
                     Pagination,
                     Pagination.sleep.name
                 ).resolves()
 
+                // independente da msg que vier da biblioteca rejeitamos o erro 
                 sandbox.stub(
                     pagination.request,
                     pagination.request.makeRequest.name
                 ).rejects(error)
 
                 const dataRequest = { url: 'https://google.com', page: 0 }
+                // valida quando der uma exceção 
                 await assert.rejects(pagination.handleRequest(dataRequest), error)
+                // tem que ser chamado 2 vezes
                 assert.deepStrictEqual(pagination.handleRequest.callCount, expectedCallCount)
 
                 const lastCall = 1
                 const firstCallArg = pagination.handleRequest.getCall(lastCall).firstArg
+                // pega a ultima vez que retries foi chamado
                 const firstCallRetries = firstCallArg.retries
+                // faz o teste verifica se passou duas vezes
                 assert.deepStrictEqual(firstCallRetries, expectedCallCount)
 
+                // queremos validar se o makerequest esta passado de forma certa
                 const expectedArgs = {
                     url: `${dataRequest.url}?tid=${dataRequest.page}`,
                     method: 'get',
                     timeout: expectedTimeout
                 }
-                const firstCallArgs = pagination.request.makeRequest.getCall(0).args
-                assert.deepStrictEqual(firstCallArgs, [expectedArgs])
 
+                // pega a chamada do makerequest 
+                const firstCallArgs = pagination.request.makeRequest.getCall(0).args
+                // faz o teste e verifica se é igual expectedargs 
+                assert.deepStrictEqual(firstCallArgs, [expectedArgs])
+                // valida se o sleep foi chamado com os parametros que a gente passou
                 assert.ok(Pagination.sleep.calledWithExactly(expectedTimeout))
             })
-
-            it('should return data from request when succeded', async () => {
+            // deve retornar dados da solicitação quando bem-sucedida
+            it('should return data from request when succeeded', async () => {
                 const data = { result: 'ok' }
                 const pagination = new Pagination()
                 sandbox.stub(
@@ -119,6 +135,8 @@ describe('Pagination tests', () => {
                 ).resolves(data)
 
                 const result = await pagination.handleRequest({ url: 'https://google.com', page: 1 })
+                
+                // faz o teste 
                 assert.deepStrictEqual(result, data)
             })
         })
@@ -140,7 +158,7 @@ describe('Pagination tests', () => {
                     "amount": 0.3
                 }
             ]
-
+            //deve atualizar o id do pedido em cada pedido
             it('should update request id on each request', async () => {
                 const pagination = new Pagination()
                 sandbox.stub(
@@ -177,13 +195,15 @@ describe('Pagination tests', () => {
                 // r.next() => { done: true | false, value: {} }
                 // a melhor forma é usar o for of
                 const gen = pagination.getPaginated(data)
-                for await (const result of gen) { }
+                // para cada yield vai cair dentro do for, dessa forma conseguimos pegar as chamadas
+                for await (const result of gen) {}
 
                 const getFirstArgFromCall = value => pagination.handleRequest.getCall(value).firstArg
                 assert.deepStrictEqual(getFirstArgFromCall(0), data)
                 assert.deepStrictEqual(getFirstArgFromCall(1), secondCallExpectation)
                 assert.deepStrictEqual(getFirstArgFromCall(2), thirdCallExpectation)
             })
+            // deve parar de solicitar quando a solicitação retornar uma matriz vazia
             it('should stop requesting when request return an empty array', async () => {
                 const expectedThreshold = 20
                 const pagination = new Pagination()
@@ -211,9 +231,11 @@ describe('Pagination tests', () => {
                 ])
 
                 const expectedFirstCall = { done: false, value: [responseMock[0]]}
+                // validando o yield sem *
                 assert.deepStrictEqual(firstResult, expectedFirstCall)
-                
+        
                 const expectedSecondCall = { done: true, value: undefined }
+                 // na segunda vez o yield*
                 assert.deepStrictEqual(secondResult, expectedSecondCall)
 
                 assert.deepStrictEqual(Pagination.sleep.callCount, 1)
